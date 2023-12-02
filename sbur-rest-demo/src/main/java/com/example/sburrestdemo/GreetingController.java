e88568b7baf22cb57c5a96dc660d78599987d5ff
macho199 <macho199@gmail.com>
2023-12-02 18:03:20 +0900

    add ConfigurationProperty

---
+++ sbur-rest-demo/src/main/java/com/example/sburrestdemo/GreetingController.java
@@ -0,0 +1,28 @@
+package com.example.sburrestdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sburrestdemo.model.Greeting;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    private final Greeting greeting;
+
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
+    @GetMapping
+    String getGreeting() {
+        return greeting.getName();
+    }
+
+
+    @GetMapping("/coffee")
+    String getNameAndCoffee() {
+        return greeting.getCoffee();
+    }
+}
