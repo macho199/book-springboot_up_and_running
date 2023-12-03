68f46c5b856faf326f16cdda827b900f311ed553
macho199 <macho199@gmail.com>
2023-12-03 15:24:11 +0900

    create planfinder project

---
+++ planfinder/src/test/java/com/example/planfinder/PlanfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
