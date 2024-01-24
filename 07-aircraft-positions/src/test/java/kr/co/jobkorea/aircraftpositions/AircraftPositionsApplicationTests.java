95c9e9d144e2a32fbb1ad5bb4e92351b1df0acfb
Kwon Jong Seong <kjsdev@jobkorea.co.kr>
2024-01-24 21:08:02 +0900

    init

---
+++ 07-aircraft-positions/src/test/java/kr/co/jobkorea/aircraftpositions/AircraftPositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jobkorea.aircraftpositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftPositionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- 09-data-jpa-mongodb/src/main/kotlin/com/example/demo/repository/AircraftRepository.kt
+++
@@ -1,3 +0,0 @@
-import org.springframework.data.repository.CrudRepository
-
-interface AircraftRepository: CrudRepository<Aircraft, String>
