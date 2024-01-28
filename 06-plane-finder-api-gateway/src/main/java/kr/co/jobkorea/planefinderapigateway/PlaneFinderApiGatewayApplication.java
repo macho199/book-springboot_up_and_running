17c921ebc7f9c7b69f3508172cf5d4a768049a98
macho199 <macho199@gmail.com>
2024-01-28 16:36:07 +0900

    init project 06-plane-finder-api-gateway

---
+++ 06-plane-finder-api-gateway/src/main/java/kr/co/jobkorea/planefinderapigateway/PlaneFinderApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jobkorea.planefinderapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneFinderApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneFinderApiGatewayApplication.class, args);
+	}
+
+}
---
+++ 06-plane-finder-api-gateway/src/test/java/kr/co/jobkorea/planefinderapigateway/PlaneFinderApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jobkorea.planefinderapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinderApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
