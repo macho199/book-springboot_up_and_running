142112c929a00eedd5df3a018458e16c4b1300f2
macho199 <macho199@gmail.com>
2024-01-28 16:49:19 +0900

    add

--- 06-plane-finder-api-gateway/src/main/java/kr/co/jobkorea/planefinderapigateway/PlaneFinderApiGatewayApplication.java
+++ 06-plane-finder-api-gateway/src/main/java/kr/co/jobkorea/planefinderapigateway/PlaneFinderApiGatewayApplication.java
@@ -2,9 +2,19 @@ package kr.co.jobkorea.planefinderapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import kr.co.jobkorea.planefinderapigateway.model.Aircraft;
 
 @SpringBootApplication
 public class PlaneFinderApiGatewayApplication {
+	@Bean
+	public RedisOperations<String, Aircraft> redisOperations(RedisConnectionFactory factory) {
+		Jackson2JsonRedisSerializer<Aircraft> serializer = new Jackson2JsonRedisSerializer<>(Aircraft.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaneFinderApiGatewayApplication.class, args);
