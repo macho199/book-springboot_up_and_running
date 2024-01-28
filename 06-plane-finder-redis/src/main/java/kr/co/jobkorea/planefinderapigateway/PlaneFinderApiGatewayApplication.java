e449b4a0f422624845b6d5384940467f52187893
kjs <macho199@gmail.com>
2024-01-28 20:04:24 +0900

    change

--- 06-plane-finder-redis/src/main/java/kr/co/jobkorea/planefinderapigateway/PlaneFinderApiGatewayApplication.java
+++ 06-plane-finder-redis/src/main/java/kr/co/jobkorea/planefinderapigateway/PlaneFinderApiGatewayApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import kr.co.jobkorea.planefinderapigateway.model.Aircraft;
 
@@ -14,6 +16,12 @@ public class PlaneFinderApiGatewayApplication {
 	@Bean
 	public RedisOperations<String, Aircraft> redisOperations(RedisConnectionFactory factory) {
 		Jackson2JsonRedisSerializer<Aircraft> serializer = new Jackson2JsonRedisSerializer<>(Aircraft.class);
+		RedisTemplate<String, Aircraft> template = new RedisTemplate<>();
+		template.setConnectionFactory(factory);
+		template.setDefaultSerializer(serializer);
+		template.setKeySerializer(new StringRedisSerializer());
+
+		return template;
 	}
 
 	public static void main(String[] args) {
---
+++ 06-plane-finder/src/test/java/kr/co/jobkorea/planefinder/PlaneFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jobkorea.planefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
