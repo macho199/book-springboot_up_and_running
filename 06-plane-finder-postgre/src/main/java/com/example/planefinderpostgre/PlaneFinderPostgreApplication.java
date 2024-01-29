8dc12b66335171cd0669edae799a55f57bdd8434
kjs <macho199@gmail.com>
2024-01-29 15:44:27 +0900

    init 06-plane-finder-postgre

---
+++ 06-plane-finder-postgre/src/main/java/com/example/planefinderpostgre/PlaneFinderPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.planefinderpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneFinderPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneFinderPostgreApplication.class, args);
+	}
+
+}
---
+++ 06-plane-finder-postgre/src/test/java/com/example/planefinderpostgre/PlaneFinderPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planefinderpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinderPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
