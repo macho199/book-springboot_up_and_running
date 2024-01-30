f7caed9ec69a13f8779096198150fab1610f7390
Kwon Jong Seong <kjsdev@jobkorea.co.kr>
2024-01-30 18:43:58 +0900

    add

---
+++ 06-plane-finder/src/main/java/kr/co/jobkorea/planefinder/PlaneRepository.java
@@ -0,0 +1,7 @@
+package kr.co.jobkorea.planefinder;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlaneRepository extends CrudRepository<Aircraft, Long>{
+    
+}
